8533512828f83749768d2e63bdee5bed027b1a62
Joseph Kato <joseph@jdkato.io>
2026-01-23 03:47:01 -0800

    chore: fix golangci-lint
    Signed-off-by: Joseph Kato <joseph@jdkato.io>

--- internal/system/path_test.go
+++ internal/system/path_test.go
@@ -1,40 +1,40 @@
 package system
 
 import (
-   "path/filepath"
-   "testing"
+	"path/filepath"
+	"testing"
 )
 
 func TestDeterminePath(t *testing.T) {
-   tests := []struct {
-       configPath string
-       keyPath    string
-       expected   string
-   }{
-       {
-           configPath: "C:\\Source\\project\\.vale.ini",
-           keyPath:    "transform.xsl",
-           expected:   "C:\\Source\\project\\transform.xsl",
-       },
-       {
-           configPath: "C:\\Source\\project\\.vale.ini",
-           keyPath:    "styles/transform.xsl",
-           expected:   "C:\\Source\\project\\styles\\transform.xsl",
-       },
-       {
-           configPath: "C:\\Source\\project\\.vale.ini",
-           keyPath:    "C:\\Other\\transform.xsl",
-           expected:   "C:\\Other\\transform.xsl",
-       },
-   }
+	tests := []struct {
+		configPath string
+		keyPath    string
+		expected   string
+	}{
+		{
+			configPath: "C:\\Source\\project\\.vale.ini",
+			keyPath:    "transform.xsl",
+			expected:   "C:\\Source\\project\\transform.xsl",
+		},
+		{
+			configPath: "C:\\Source\\project\\.vale.ini",
+			keyPath:    "styles/transform.xsl",
+			expected:   "C:\\Source\\project\\styles\\transform.xsl",
+		},
+		{
+			configPath: "C:\\Source\\project\\.vale.ini",
+			keyPath:    "C:\\Other\\transform.xsl",
+			expected:   "C:\\Other\\transform.xsl",
+		},
+	}
 
-   for _, tt := range tests {
-       actual := DeterminePath(tt.configPath, tt.keyPath)
-       // Clean the paths to ensure separators match for comparison
-       actual = filepath.Clean(actual)
-       expected := filepath.Clean(tt.expected)
-       if actual != expected {
-           t.Errorf("DeterminePath(%q, %q) = %q; want %q", tt.configPath, tt.keyPath, actual, expected)
-       }
-   }
+	for _, tt := range tests {
+		actual := DeterminePath(tt.configPath, tt.keyPath)
+		// Clean the paths to ensure separators match for comparison
+		actual = filepath.Clean(actual)
+		expected := filepath.Clean(tt.expected)
+		if actual != expected {
+			t.Errorf("DeterminePath(%q, %q) = %q; want %q", tt.configPath, tt.keyPath, actual, expected)
+		}
+	}
 }
